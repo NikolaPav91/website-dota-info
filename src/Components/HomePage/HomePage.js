9959cfc2eb260a67fc8c553673e88056babfb255
Nikola Pavlovic <svvaba@gmail.com>
2018-10-02 16:33:15 +0200

    working on design

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import YouTube from 'react-youtube';
 
 class HomePage extends React.Component {
   componentDidMount() {
@@ -10,6 +11,21 @@ class HomePage extends React.Component {
     return (
       <div className="content-container-home">
       <header className="header-picture1"></header>
+      <div id="home-container-bg01" className="Container-bg">
+        <div id="home-inner-bg01">
+        <div className="Content-container-home" id="content-container-home01">
+          <div id="content-home01">
+          <h2>The most-played game on Steam.</h2>
+        </div>
+        </div>
+      </div>
+      </div>
+      <div id="home-container-bg02" className="Container-bg"></div>
+      <div id="home-container-bg03" className="Container-bg"></div>
+
+      <YouTube
+        videoId="SmnqsdeHFT0"
+      />
       <p className="App-intro">
               This is the Home page.
             </p>
