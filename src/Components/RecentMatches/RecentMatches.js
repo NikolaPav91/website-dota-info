c1f73e891c37df9ed4b4ae84fc3c2620ec9f2281
Nikola Pavlovic <svvaba@gmail.com>
2018-10-13 19:38:33 +0200

    result container done but not designed

--- src/Components/RecentMatches/RecentMatches.js
+++ src/Components/RecentMatches/RecentMatches.js
@@ -1,58 +1,77 @@
 import React from 'react';
+import './RecentMatches.css';
+import classNames from 'classnames';
 
 class RecentMatches extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       latestMatches:[],
+      direTags:[]
     }
   }
 
   componentDidMount() {
+    let bla;
     this.setState({
       loaderActive: true,
     });
     fetch('https://api.opendota.com/api/proMatches')
-    .then(response=> {console.log(response.status); return response})
     .then(response=> response.json())
     .then(response=>
       response
-      .slice(0,10)
-      .map((item,index)=>{return {id: item["match_id"], startTime:item["start_time"],
-      radiantTeamId:item["radiant_team_id"], radiantTeamName: item["radiant_name"],
-      direTeamId: item["dirte_team_id"], direTeamName: item["dire_name"], radiantWin: item["radiant_win"],
+      .filter(item=>(item.leagueid===10332 || item.leagueid===10269))
+      .map((item,index)=>{return {id: item["match_id"]
     }})
+    )
+    .then(response=>
+      response
+      .map((item)=>
+        fetch('https://api.opendota.com/api/matches/'+ item.id)
+        .then(response=> response.json())
+
+    )
+
+    )
+    .then(async response=> await Promise.all(response)
     )
     .then(response=> this.setState({
       latestMatches: response,
-      loaderActive: false,
     }))
 
   }
     render() {
       let matches= this.state.latestMatches;
       let showmatches= matches.map((item,index)=> {
+        let resultcontainerclass=classNames({
+          'Result-container': true,
+          'Even-row': index%2===0,
+          'Uneven-row': index % 2 ===1,
+        })
         let result= "1 : 0";
-        if (item.radiantWin===false) {
+        if (item["radiant_win"]===false) {
           result="0 : 1"
         }
-        let radiantname= item.radiantTeamName;
-        let direname= item.direTeamName;
-        if (radiantname===null) {
-          radiantname="Name Unknown"
+        let radiantname= "Name unknown"
+        if  (item["radiant_team"]===undefined){ radiantname="Tag Unknown" } else {radiantname=item["radiant_team"].tag}
+        let direname;
+        if  (item["dire_team"]===undefined){ direname="Tag Unknown" } else {direname=item["dire_team"].tag}
+        if (!radiantname) {
+          radiantname=item["radiant_team"].name
         }
-        if (direname===null) {
-          direname="Name Unknown"
+        if (!direname) {
+          direname=item["dire_team"].name
         }
 
         return (
-          <div className="Result-container">{direname} {result} {radiantname}</div>
+          <div className={resultcontainerclass}>{direname} {result} {radiantname}</div>
         )
       }
     )
       return (
         <div id="recent-matches-container">
           {showmatches}
+          {this.state.direTags}
         </div>
     )
   }
