6ddd039b3c161cde32b9141455b29913e49c42a6
Nikola Pavlovic <svvaba@gmail.com>
2018-10-14 14:17:55 +0200

    improving news page(result container)

--- src/Components/RecentMatches/RecentMatches.js
+++ src/Components/RecentMatches/RecentMatches.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './RecentMatches.css';
 import classNames from 'classnames';
+import Loader from '../Loader/Loader';
 
 class RecentMatches extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       latestMatches:[],
-      direTags:[]
     }
   }
 
@@ -20,7 +20,8 @@ class RecentMatches extends React.PureComponent {
     .then(response=> response.json())
     .then(response=>
       response
-      .filter(item=>(item.leagueid===10332 || item.leagueid===10269))
+      .filter(item=>(item.leagueid===10332 || item.leagueid===10269 || item.leagueid===10145))
+      .slice(0,32 )
       .map((item,index)=>{return {id: item["match_id"]
     }})
     )
@@ -36,11 +37,16 @@ class RecentMatches extends React.PureComponent {
     .then(async response=> await Promise.all(response)
     )
     .then(response=> this.setState({
+      loaderActive: false,
       latestMatches: response,
     }))
 
   }
     render() {
+      let loaderclass= classNames({
+        'Loader': this.state.loaderActive,
+        'Display-none': !this.state.loaderActive
+      })
       let matches= this.state.latestMatches;
       let showmatches= matches.map((item,index)=> {
         let resultcontainerclass=classNames({
@@ -52,10 +58,12 @@ class RecentMatches extends React.PureComponent {
         if (item["radiant_win"]===false) {
           result="0 : 1"
         }
-        let radiantname= "Name unknown"
-        if  (item["radiant_team"]===undefined){ radiantname="Tag Unknown" } else {radiantname=item["radiant_team"].tag}
-        let direname;
-        if  (item["dire_team"]===undefined){ direname="Tag Unknown" } else {direname=item["dire_team"].tag}
+        let radiantname= "??"
+        let radiantlogosrc="no-image-icon.png"
+        if  (item["radiant_team"]){radiantname=item["radiant_team"].tag; radiantlogosrc=item["radiant_team"]["logo_url"]}
+        let direname= "??"
+        let direlogosrc="no-image-icon.png";
+        if  (item["dire_team"]){direname=item["dire_team"].tag; direlogosrc=item["dire_team"]["logo_url"]}
         if (!radiantname) {
           radiantname=item["radiant_team"].name
         }
@@ -63,15 +71,21 @@ class RecentMatches extends React.PureComponent {
           direname=item["dire_team"].name
         }
 
+
+
         return (
-          <div className={resultcontainerclass}>{direname} {result} {radiantname}</div>
+          <div className={resultcontainerclass}>
+            <div className="Result-radiant-team"><img className="Result-teamlogo" src={radiantlogosrc}></img> <span>{radiantname}</span> </div>
+          {result}
+            <div className="Result-dire-team"><span>{direname}</span> <img className="Result-teamlogo" src={direlogosrc}></img> </div>
+        </div>
         )
       }
     )
       return (
         <div id="recent-matches-container">
+          <Loader className={loaderclass}></Loader>
           {showmatches}
-          {this.state.direTags}
         </div>
     )
   }
